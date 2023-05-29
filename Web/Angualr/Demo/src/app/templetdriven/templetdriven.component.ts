039af2af564b0f2be626739165fab0f2156d20b8
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2023-05-29 10:20:18 +0530

    validation code

--- Web/Angualr/Demo/src/app/templetdriven/templetdriven.component.ts
+++ Web/Angualr/Demo/src/app/templetdriven/templetdriven.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./templetdriven.component.css']
 })
 export class TempletdrivenComponent {
-  userdate(userForm:any){
-    
-    console.log(userForm);
-}
+  
+  userdata(userform:any){
+    console.log(userform)
+  }
 }
